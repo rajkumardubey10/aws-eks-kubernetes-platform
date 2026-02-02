67b75127144c8c954f11b3bc2045ddfb4f1d05e9
rajkumardubey10 <dubeyrajkumar@683gmail.com>
2026-02-02 09:53:53 +0000

    remote-backend.tf file added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "remote-backend-bucket-123"
-#     key    = "s3/terraform.tfstate"
-#     region = "ap-south-1"
-#     use_lockfile = true
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "remote-backend-bucket-jenkins-159"
+    key    = "s3/terraform.tfstate"
+    region = "ap-south-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
---
+++ terraform/output.tf
@@ -0,0 +1,57 @@
+########################
+# VPC Outputs
+########################
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+########################
+# EKS Outputs
+########################
+
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = module.eks-cluster-application.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS Cluster API Server Endpoint"
+  value       = module.eks-cluster-application.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "EKS Cluster CA Certificate"
+  value       = module.eks-cluster-application.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "cluster_arn" {
+  description = "EKS Cluster ARN"
+  value       = module.eks-cluster-application.cluster_arn
+}
+
+output "oidc_provider_arn" {
+  description = "OIDC Provider ARN (for IRSA)"
+  value       = module.eks-cluster-application.oidc_provider_arn
+}
+
+########################
+# Node Group Outputs
+########################
+
+output "node_group_iam_role_arn" {
+  description = "IAM Role ARN for worker nodes"
+  value       = module.eks-cluster-application.eks_managed_node_groups["default"].iam_role_arn
+}
